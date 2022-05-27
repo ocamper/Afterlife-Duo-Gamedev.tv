cde45c18a6acbb123402dfe179c18428c6375754
ocamper <danielocampolozano@gmail.com>
2022-05-26 22:43:39 -0500

    Title Screen
    Title Screen was added; some improvements might be done later down the line but this is good enough for now (and also good enough for submission if I do not have time to polish further)

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject outScreen;
+    [SerializeField] private GameObject stButton;
+    [SerializeField] private AudioSource swishSfx;
+    [SerializeField] private AudioSource textDoneSfx;
+    [SerializeField] private AudioSource titleMusic;
+    [SerializeField] private GameObject creditButton;
+    [SerializeField] private GameObject creditUi;
+    private bool musicPlayed;
+
+    public void OnClick()
+    {
+        outScreen.SetActive(true);
+    }
+
+    public void SpawnButton()
+    {
+        stButton.SetActive(true);
+        creditButton.SetActive(true);
+        if (!musicPlayed)
+            titleMusic.Play();
+
+        musicPlayed = true;
+    }
+
+    public void WindEffect()
+    {
+        swishSfx.Play();
+    }
+
+    public void newSfx()
+    {
+        textDoneSfx.Play();
+    }
+
+    public void CreditsOn()
+    {
+        creditUi.SetActive(true);
+    }
+
+    public void CreditsOff()
+    {
+        creditUi.SetActive(false);
+    }
+}
